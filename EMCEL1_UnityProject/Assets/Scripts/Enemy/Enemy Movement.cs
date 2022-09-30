1712384bbc1182efe7cbe9c309bc87f19ec196a4
Nova <novalostj@gmail.com>
2022-09-30 10:10:28 +0800

    Sending New Files

--- EMCEL1_UnityProject/Assets/Scripts/Enemy/Enemy Movement.cs
+++ EMCEL1_UnityProject/Assets/Scripts/Enemy/Enemy Movement.cs
@@ -2,23 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class Player
+public class EnemyMovement : MonoBehaviour
 {
-    
-}
-
-public class Enemy
-{
-    
-}
-public class sample : MonoBehaviour
-{
-    
-
-
-
-
     // Start is called before the first frame update
     void Start()
     {
