36a39a1c3ba27ec92c9c9bd23dfd294f1fa7bba3
FrustratedJeh <80657334+novalostj@users.noreply.github.com>
2022-10-23 08:25:26 +0800

    Removal of unneceassry files

--- EMCEL1_UnityProject/Assets/Scripts/UI/In Game UI/HealthBar.cs
+++ EMCEL1_UnityProject/Assets/Scripts/UI/In Game UI/HealthBar.cs
@@ -1,9 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI.In_Game_UI
 {
     public class HealthBar : MonoBehaviour
     {
+        public Image hpBar;
         
+        public float playerHealth = 100f;
+        public float playerMaxHealth = 100f;
+
+        public Color healthyColor, dyingColor;
+
+        private void Update()
+        {
+            float value = playerHealth / playerMaxHealth;
+            
+            hpBar.fillAmount = value;
+            hpBar.color = Color.Lerp(dyingColor, healthyColor, value);
+        }
     }
 }
