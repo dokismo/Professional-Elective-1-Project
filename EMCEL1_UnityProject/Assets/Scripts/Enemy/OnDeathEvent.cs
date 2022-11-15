82e5c44010dedb38c2e71fed1aeeef2b96c44679
codeloco07 <joshua.delapena@neu.edu.ph>
2022-11-15 13:24:10 +0800

    sfx_update
    zombie sfx
    bgm
    fix scripts

--- EMCEL1_UnityProject/Assets/Scripts/Enemy/OnDeathEvent.cs
+++ EMCEL1_UnityProject/Assets/Scripts/Enemy/OnDeathEvent.cs
@@ -8,11 +8,12 @@ namespace Enemy
         public delegate void OnDeath(int money);
         public static OnDeath givePlayerMoney;
 
+       
         public int deathValue = 100;
 
         private void OnDestroy()
         {
-            //FindObjectType<AudioManager>().Play("zombie_sfx1");
+            FindObjectOfType<AudioManager>().Play("zombie_sfx1");
             givePlayerMoney?.Invoke(deathValue);
         }
     }
