b18b239f9bde36f72f16ed961e972960a2d68297
codeloco07 <joshua.delapena@neu.edu.ph>
2022-11-15 10:30:12 +0800

    sfx update

--- EMCEL1_UnityProject/Assets/Scripts/SFX/AudioManager.cs
+++ EMCEL1_UnityProject/Assets/Scripts/SFX/AudioManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
+   
 
     // Start is called before the first frame update
     void Awake()
@@ -22,7 +23,7 @@ public class AudioManager : MonoBehaviour
     // Update is called once per frame
     public void Play (string name)
     {
-        Array.find(sounds, sound => sound.name == name);
+        Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.Play();
     }
 }
