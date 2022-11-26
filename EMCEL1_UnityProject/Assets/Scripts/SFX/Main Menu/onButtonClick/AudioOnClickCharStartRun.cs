00f93f65e29ededbaa856e0283b290f7c039d6d7
codeloco07 <joshua.delapena@neu.edu.ph>
2022-11-26 12:07:00 +0800

    SFX (Main Menu UI SFX Revised Script)
    Summary of the Update:
    - Made some changes in scripts for buttons instead playing from game object audio sources, it plays a sound when its called out.
    - Functional Audio Manager for the entire Main Menu UI.
    - Future fixes
    
    Later to add:
    - 2 Main Menu Music

---
+++ EMCEL1_UnityProject/Assets/Scripts/SFX/Main Menu/onButtonClick/AudioOnClickCharStartRun.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AudioOnClickCharStartRun : MonoBehaviour
+{
+	public Button ButtonsUI;
+
+	void Start()
+	{
+		Button btn = ButtonsUI.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		Debug.Log("Sound Plays");
+		FindObjectOfType<AudioManager>().Play("clickStartRun");//sfx
+	}
+}
